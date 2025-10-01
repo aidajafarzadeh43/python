4482f957dfbe011f959ac11194ad3a4ec04cefa9
aida <aidajafarzadeh43@gmail.com>
2025-10-01 11:27:18 +0330

    numbers

---
+++ 1.py
@@ -0,0 +1,16 @@
+# frist_number = input ("choose a number: ")
+# frist_number = int (frist_number)
+# number = frist_number
+# number *= 2
+# number += 8
+# number += frist_number
+# number -= 2
+# number /= 3
+# number -= frist_number
+# number *= 4
+# print (number)
+
+# num = 1
+# while num < 6:
+#    print (num)
+#    num += 1
