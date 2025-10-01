cca9c1f76640b206b26724d1f1a44d816bb6d763
aida <aidajafarzadeh43@gmail.com>
2025-10-01 10:56:12 +0330

    learn python

---
+++ 2.py
@@ -0,0 +1,20 @@
+# def name (a,b) :
+
+#     print(f"hello {a}")
+#     print (f"hello {b}")
+
+# name("reza" , "aida")
+
+
+
+def number_type(number):
+    if number % 2 == 0:
+        print("this is in number even")
+    else:
+      #   print("this is in number odd")
+     pass
+number = int(input('enter your number :' ))
+number_type(number)
+   
+
+
