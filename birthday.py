5815c2c04d0415e4cce07e546ade84c8827c0328
aida <aidajafarzadeh43@gmail.com>
2025-10-04 10:56:30 +0330

    birthday

--- birthday.py
+++ birthday.py
@@ -11,17 +11,4 @@ year = int(input('enter year:'))
 
 convert(day, month, year)
    
-# def convert(day, month, year):
-#     if month > 10 or (month == 10 and day > 10):
-#         birthday = year + 622
-#     else:
-#         birthday = year + 621
-#     print(f"your birthday year is {birthday}")
 
-# # گرفتن ورودی از کاربر
-# day = int(input('enter day: '))
-# month = int(input('enter month: '))
-# year = int(input('enter year: '))
-
-# # فراخوانی تابع با مقادیر عددی
-# convert(day, month, year)
