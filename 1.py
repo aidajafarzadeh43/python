9d545edd4577cf934412e9649dc0a3d7e8ca11a6
aidajafarzadeh43 <aidajafarzadeh43@gmail.com>
2025-10-04 09:26:17 +0330

    Update 1.py

--- 1.py
+++ 1.py
@@ -1,16 +1,18 @@
-# frist_number = input ("choose a number: ")
-# frist_number = int (frist_number)
-# number = frist_number
-# number *= 2
-# number += 8
-# number += frist_number
-# number -= 2
-# number /= 3
-# number -= frist_number
-# number *= 4
-# print (number)
+frist_number = input ("choose a number: ")
+frist_number = int (frist_number)
+number = frist_number
+number *= 2
+number += 8
+number += frist_number
+number -= 2
+number /= 3
+number -= frist_number
+number *= 4
+print (number)
 
-# num = 1
-# while num < 6:
-#    print (num)
-#    num += 1
+
+
+num = 1
+while num < 6:
+   print (num)
+   num += 1
